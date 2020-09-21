a5d099a53a93b9f1d937e85209a35881ebc53378
Rudy Jdira <rjdira@talend.com>
2020-09-21 16:26:21 +0200

    move Build to build.sbt

--- project/Build.scala
+++ project/Build.scala
@@ -1,85 +1,5 @@
-import sbt._
 import sbt.Keys._
-import sbtfilter.Plugin.FilterKeys._
-//import scoverage.ScoverageSbtPlugin._
-
-object Build extends sbt.Build {
-
-  lazy val scala212 = "2.12.11"
-  lazy val scala211 = "2.11.12"
-  lazy val supportedScalaVersions = List(scala212, scala211)
-
-  lazy val avpath = Project("wandou-avpath", file("."))
-    .settings(basicSettings: _*)
-    .settings(Formatting.settings: _*)
-    .settings(Formatting.buildFileSettings: _*)
-    .settings(releaseSettings: _*)
-    .settings(sbtrelease.ReleasePlugin.releaseSettings: _*)
-    .settings(libraryDependencies ++= Dependencies.avro ++ Dependencies.test)
-    .settings(Packaging.settings: _*)
-    .settings(sbtavro.SbtAvro.avroSettings ++ avroSettingsTest: _*)
-    //.settings(instrumentSettings: _*)
-    .settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*)
-    .settings(crossScalaVersions := supportedScalaVersions)
-
-  lazy val basicSettings = Seq(
-    organization := "com.wandoulabs.avro",
-    version := "0.1.6-talend-SNAPSHOT",
-    scalacOptions ++= Seq("-unchecked", "-deprecation"),
-    resolvers ++= Seq(
-      "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases",
-      "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-      "Typesafe repo" at "https://repo.typesafe.com/typesafe/releases/"),
-    javacOptions ++= Seq("-source", "1.6", "-target", "1.6"))
-
-  lazy val avroSettings = Seq(
-    sbtavro.SbtAvro.stringType in sbtavro.SbtAvro.avroConfig := "String",
-    sourceDirectory in sbtavro.SbtAvro.avroConfig <<= (resourceDirectory in Compile)(_ / "avsc"),
-    version in sbtavro.SbtAvro.avroConfig := "1.8.2")
-
-  // Todo rewrite sbt-avro to compile in Test phase.
-  lazy val avroSettingsTest = Seq(
-    sbtavro.SbtAvro.stringType in sbtavro.SbtAvro.avroConfig := "String",
-    sourceDirectory in sbtavro.SbtAvro.avroConfig <<= (resourceDirectory in Test)(_ / "avsc"),
-    javaSource in sbtavro.SbtAvro.avroConfig <<= (sourceManaged in Test)(_ / "java" / "compiled_avro"),
-    version in sbtavro.SbtAvro.avroConfig := "1.8.2")
-
-  lazy val releaseSettings = Seq(
-    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
-    publishTo := {
-      if (isSnapshot.value)
-        Some("talend_nexus_deployment" at "https://artifacts-oss.talend.com/nexus/content/repositories/TalendOpenSourceSnapshot")
-      else
-        Some("talend_nexus_deployment"  at "https://artifacts-oss.talend.com/nexus/content/repositories/TalendOpenSourceRelease")
-    },
-    publishMavenStyle := true,
-    publishArtifact in Test := false,
-    pomIncludeRepository := { (repo: MavenRepository) => false },
-    pomExtra := pomXml)
-
-  lazy val pomXml =
-    (<url>https://github.com/wandoulabs/avpath</url>
-     <licenses>
-       <license>
-         <name>Apache License 2.0</name>
-         <url>http://www.apache.org/licenses/</url>
-         <distribution>repo</distribution>
-       </license>
-     </licenses>
-     <scm>
-       <url>git@github.com:talend/avpath.git</url>
-       <connection>scm:git:git@github.com:talend/avpath.git</connection>
-     </scm>)
-
-  lazy val noPublishing = Seq(
-    publish := (),
-    publishLocal := (),
-    // required until these tickets are closed https://github.com/sbt/sbt-pgp/issues/42,
-    // https://github.com/sbt/sbt-pgp/issues/36
-    publishTo := None
-  )
-
-}
+import sbt._
 
 object Dependencies {
   val SLF4J_VERSION = "1.7.24"
@@ -103,6 +23,7 @@ object Dependencies {
 }
 
 object Formatting {
+
   import com.typesafe.sbt.SbtScalariform
   import com.typesafe.sbt.SbtScalariform.ScalariformKeys
   import ScalariformKeys._
@@ -114,16 +35,16 @@ object Formatting {
   val buildFileSettings: Seq[Setting[_]] = SbtScalariform.noConfigScalariformSettings ++
     inConfig(BuildConfig)(SbtScalariform.configScalariformSettings) ++
     inConfig(BuildSbtConfig)(SbtScalariform.configScalariformSettings) ++ Seq(
-      scalaSource in BuildConfig := baseDirectory.value / "project",
-      scalaSource in BuildSbtConfig := baseDirectory.value,
-      includeFilter in (BuildConfig, format) := ("*.scala": FileFilter),
-      includeFilter in (BuildSbtConfig, format) := ("*.sbt": FileFilter),
-      format in BuildConfig := {
-        val x = (format in BuildSbtConfig).value
-        (format in BuildConfig).value
-      },
-      ScalariformKeys.preferences in BuildConfig := formattingPreferences,
-      ScalariformKeys.preferences in BuildSbtConfig := formattingPreferences)
+    scalaSource in BuildConfig := baseDirectory.value / "project",
+    scalaSource in BuildSbtConfig := baseDirectory.value,
+    includeFilter in(BuildConfig, format) := ("*.scala": FileFilter),
+    includeFilter in(BuildSbtConfig, format) := ("*.sbt": FileFilter),
+    format in BuildConfig := {
+      val x = (format in BuildSbtConfig).value
+      (format in BuildConfig).value
+    },
+    ScalariformKeys.preferences in BuildConfig := formattingPreferences,
+    ScalariformKeys.preferences in BuildSbtConfig := formattingPreferences)
 
   val settings = SbtScalariform.scalariformSettings ++ Seq(
     ScalariformKeys.preferences in Compile := formattingPreferences,
@@ -141,13 +62,12 @@ object Formatting {
 }
 
 object Packaging {
+
   import com.typesafe.sbt.SbtNativePackager._
-  import com.typesafe.sbt.packager.Keys._
-  import com.typesafe.sbt.packager.archetypes._
 
   val settings = packagerSettings ++ deploymentSettings ++
     packageArchetype.java_application ++ Seq(
-      name := "wandou-avpath",
-      NativePackagerKeys.packageName := "wandou-avpath")
+    name := "wandou-avpath",
+    NativePackagerKeys.packageName := "wandou-avpath")
 }
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -11,6 +11,3 @@ addSbtPlugin("com.cavorite" % "sbt-avro" % "0.3.2")
 addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8.3")
 
 addSbtPlugin("com.github.sdb" % "xsbt-filter" % "0.4")
-
-// addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "1.4.1")
-
